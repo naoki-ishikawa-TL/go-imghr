61019df56da4e00c8f4f65192cc446aab64a26ee
Fumihiro Itoh <fmhrit@gmail.com>
2015-11-22 19:53:28 +0900

    divide in 3 component

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-go build -ldflags "-X main.Version `git rev-parse HEAD` -X main.LastBuild \"`date +"%Y/%m/%d %H:%M:%S"`\"" imghr.go
+go build -ldflags "-X main.Version=`git rev-parse HEAD`" imghr.go
--- imghr.go
+++ imghr.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"./amesh"
+	"./bot"
+	"./google"
+	"./jma"
 	"./slack"
-	"encoding/json"
+	"./wikipedia"
 	"log"
+	"math/rand"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
-var (
-    Version string
-    LastBuild string
+const (
+	BotName = "imghr"
+	IhrID = "U037GMSJ9"
 )
+
+var (
+	Version string
+)
+
+func imageSearch(word string) string {
+	url := google.ImageSearch(word)
+	if url == "" {
+		return "( ˘ω˘ )ｽﾔｧ"
+	} else {
+		return url + "#.png"
+	}
+}
 
 func main() {
 	token := os.Getenv("SLACK_TOKEN")
@@ -21,39 +37,115 @@ func main() {
 		log.Fatal("not set SLACK_TOKEN")
 	}
 	log.Print("init")
-	eventHandler := slack.NewEventHandler()
-	eventHandler.SetExceptionHandler(func(event slack.Event) {
-		log.Print("Unknown Event: ", event.Type)
-	})
-	messageEventHandler := slack.NewMessageEventHandler(Version, LastBuild)
-	userTypingEventHandler := slack.NewUserTypingEventHandler()
-	eventHandler.AddHandler("message", messageEventHandler.Handle)
-	eventHandler.AddHandler("user_typing", userTypingEventHandler.Handle)
-again:
-	ws := slack.ConnectSocket(token)
-	if ws == nil {
-		log.Fatal("Could not connect websocket")
-	}
-	eventChan, resultChan := slack.StartReading(ws)
-	startTime := int(time.Now().Unix())
-	for {
-		select {
-		case buf := <-eventChan:
-			var event slack.Event
-			json.Unmarshal(buf, &event)
-			if event.Ts != "" {
-				ts := strings.Split(event.Ts, ".")[0]
-				i, _ := strconv.Atoi(ts)
-				if i < startTime {
-					log.Print("skip event")
-					continue
-				}
+	ignoreUsers := []string{"U037G7YJF"}
+
+	ameshImageGenerator := amesh.NewAmeshImageGenerator()
+	jmaImageGenerator := jma.NewJmaImageGenerator()
+	lastNeteroTime := time.Now().Add(time.Duration(-13) * time.Hour)
+
+	slackConnector := slack.NewSlackConnector(token)
+	robot := bot.NewBot(slackConnector, BotName, ignoreUsers)
+	robot.Command(
+		"help",
+		"これ",
+		func(msg bot.Event) {
+			msg.Say(robot.ShowHelp())
+		},
+	)
+	robot.Command(
+		"version",
+		"show version",
+		func(msg bot.Event) {
+			msg.Say("version: "+Version)
+		},
+	)
+	robot.Command(
+		"ping",
+		"ピンポン",
+		func(msg bot.Event) {
+			msg.Say("pong")
+		},
+	)
+	robot.Command(
+		"amesh",
+		"アメッシュの画像を生成する",
+		func(msg bot.Event) {
+			targetDate := time.Now().Add(time.Duration(-1) * time.Minute).Truncate(5 * time.Minute).Format("200601021504")
+			imgPath := ameshImageGenerator.Generate(targetDate)
+
+			msg.Say("http://go-imghr.ds-12.com/" + imgPath)
+		},
+	)
+	robot.Command(
+		"jma",
+		"高解像度ナウキャストの画像を生成する",
+		func(msg bot.Event) {
+			targetDate := time.Now().UTC().Add(time.Duration(-5) * time.Minute).Truncate(5 * time.Minute).Format("200601021504")
+			imgPath := jmaImageGenerator.Generate(targetDate)
+
+				msg.Say("http://go-imghr.ds-12.com/" + imgPath)
+		},
+	)
+	robot.CommandWithArgv(
+		"img",
+		"画像検索",
+		func(msg bot.Event) {
+			msg.Say(imageSearch(msg.Argv))
+		},
+	)
+	robot.CommandWithArgv(
+		"ihr",
+		"ihrの画像検索",
+		func(msg bot.Event) {
+			words := []string{"aww cat", "柴犬", "豆柴", "aww dog", "aww shiba"}
+			msg.Say(imageSearch(words[rand.Intn(len(words))]))
+		},
+	)
+	robot.CommandWithArgv(
+		"wikipedia",
+		"wikipedia検索",
+		func(msg bot.Event) {
+			summary, err := wikipedia.GetSummary(msg.Argv)
+			if err != nil {
+				msg.Say("ページがないよ")
+			} else {
+				msg.Say(summary + "\n" + wikipedia.GenerateJaWikipediaURL(msg.Argv))
 			}
-			event.Raw = buf
-			eventHandler.Handle(event)
-		case <-resultChan:
-			log.Print("disconnect server")
-			goto again
+		},
+	)
+
+	robot.Hear("(?i)(hanakin|花金|金曜|ファナキン|tgif)", func(msg bot.Event) {
+		if time.Now().Weekday() != time.Friday {
+			return
 		}
-	}
+
+		msg.Say("花金だーワッショーイ！テンションAGEAGEマック")
+	})
+	robot.Hear("\\An\\z", func(msg bot.Event) {
+		msg.Say(imageSearch("長澤まさみ"))
+	})
+	robot.Hear("\\Ak\\z", func(msg bot.Event) {
+		msg.Say(imageSearch("木村文乃"))
+	})
+	robot.Hear("\\Aa\\z", func(msg bot.Event) {
+		msg.Say(imageSearch("有村架純"))
+	})
+	robot.Hear("\\Af\\z", func(msg bot.Event) {
+		msg.Say(imageSearch("Ferrari 458 Italia"))
+	})
+	robot.Hear("\\Ap\\z", func(msg bot.Event) {
+		msg.Say(imageSearch("Porsche 991 GT3 RS"))
+	})
+	robot.Appearance(IhrID, func(msg bot.Event) {
+		if time.Now().Sub(lastNeteroTime) < 12*time.Hour {
+			return
+		}
+		if 14 > time.Now().Hour() || time.Now().Hour() > 17 {
+			return
+		}
+		lastNeteroTime = time.Now()
+		msg.Say("I H R は 寝 て ろ ！ ！")
+	})
+
+	robot.Start()
 }
